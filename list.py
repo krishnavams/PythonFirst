252acc9aaa2984174c7e5768d65e8c7ae2f82324
krishnavams <vamsi.krishna766@gmail.com>
2022-04-30 13:09:01 +0530

    2nd

---
+++ list.py
@@ -0,0 +1,9 @@
+name_of_account_holder = input('Enter name')
+phone_number = int(input('Enter Phone Number'))
+username = input('Enter username')
+password = input('Enter password')
+account_holder_details = [name_of_account_holder, phone_number, username, password]
+print(type(name_of_account_holder))
+print(type(phone_number))
+print(type(username))
+print(type(password))
